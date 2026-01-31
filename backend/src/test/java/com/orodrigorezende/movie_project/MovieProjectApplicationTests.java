8821809ece3d250dd09e7ef934cf95e76a0bb45b
orodrigorezende <rodrigorsilva.rr94@gmail.com>
2026-01-31 15:25:07 -0300

    Project created

---
+++ backend/src/test/java/com/orodrigorezende/movie_project/MovieProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orodrigorezende.movie_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
