f9d4fbcb4b0f5708c2823e394372e5e1761479d0
orodrigorezende <rodrigorsilva.rr94@gmail.com>
2026-02-05 20:27:31 -0300

    Arrumando vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Adicione esta linha
+  // Remova o './' e deixe apenas '/' ou remova a linha 'base'
+  base: '/', 
 })
